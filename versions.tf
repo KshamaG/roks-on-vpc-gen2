7b84343bf95f724e9bea58295b55911b474461de
KshamaG <kgurudut@in.ibm.com>
2020-12-05 13:41:51 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     ibm = {
-      source  = "ibm-cloud/ibm"
+      source = "IBM-Cloud/ibm"
       version = "1.16.0"
     }
   }
